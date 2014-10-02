e90ed7f9f1c5f681de9f5aa6de1b6c72338ca76a
Mykal Cuin <mykcuin@gmail.com>
2014-10-02 13:21:14 -0600

    Worked through some of the dart side of the stopwatch.

--- stopwatch/web/stopwatch.dart
+++ stopwatch/web/stopwatch.dart
@@ -1,6 +1,38 @@
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 
+@CustomTag('tut-stopwatch')
+class TutStopwatch extends PolymerElement {
+  @observable String counter = '00:00';
+  
+  TutStopwatch.created() : super.created();
+  
+  @override
+  void attached() {
+    super.attached();
+    startButton = $['startButton'];
+    stopButton = $['stopButton'];
+    resetButton = $['resetButton'];
+    
+    stopButton.disabled = true;
+    resetButton.disabled = true;
+  }
+  
+  void updateTime(Timer _) {
+    counter = '$minute:$second';
+  }
+  
+  void start(Event e, var detail, Node target) {
+    mywatch.start();
+    var oneSecond = new Duration(seconds:1);
+    myTimer = new Timer.periodic(oneSecond, updateTimer);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = true;
+    
+  }
+}
+
 void main() {
   
 }
